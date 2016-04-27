81a9aba214cd9d618781e439e2adb2957b53043b
AlteraMobile <katherine@altera.cl>
2016-04-27 01:28:29 -0300

    2016-04-26
    Avance Slider
    Se agrega archivo masonry.js

--- src/js/unslider.js
+++ src/js/unslider.js
@@ -68,8 +68,8 @@
 			//  Either set true/false, or an object with the HTML
 			//  elements for each arrow like below:
 			arrows: {
-				prev: '<a class="' + self._ + '-arrow prev">Prev</a>',
-				next: '<a class="' + self._ + '-arrow next">Next</a>'
+				prev: '<a class="' + self._ + '-arrow prev"><img src="img/slider/arrow_left.png" width="50"></a>',
+				next: '<a class="' + self._ + '-arrow next"><img src="img/slider/arrow_right.png" width="50"></a>'
 			},
 
 			//  How should Unslider animate?
