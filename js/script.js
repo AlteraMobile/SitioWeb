63f0daa1d8576b3f3f7d1280eebc99155e5e5998
AlteraMobile <katherine@altera.cl>
2016-05-22 16:00:42 -0400

    2016-05-22
    Primera carga del dia

--- js/script.js
+++ js/script.js
@@ -1,34 +1,114 @@
 jQuery(document).ready(function($) {
 
+	//alert("ok");
+	$( "#btn_nuevo" ).click( function(){
+		$( "#form_nuevo" ).dialog({
+		    height: 250,
+		    width: 530,
+      		modal: true,
+      		resizable: true,
+      		closeOnEscape: true,
+      		
+      	});
+	});
+
+	$( ".btn_mod_actor" ).click( function(){
+		$( "#form_modificar" ).dialog({
+		    height: 250,
+		    width: 530,
+      		modal: true,
+      		resizable: true,
+      		closeOnEscape: true,
+
+      	});
+	});
+
+	$( ".btn_mod_slider" ).click( function(){
+		$( "#form_mod_slider" ).dialog({
+		    height: 200,
+		    width: 500,
+      		modal: true,
+      		resizable: true,
+      		closeOnEscape: true,
+
+      	});
+	});
+
+	$( ".btn_mod_actor" ).click( function(){
+		$( "#form_mod_pelicula" ).dialog({
+		    height: 630,
+		    width: 950,
+      		modal: true,
+      		resizable: true,
+      		closeOnEscape: true,
+
+      	});
+	});
+
+	$( "#btn_nuevo_slider" ).click( function(){
+		$( "#form_slider" ).dialog({
+		    height: 250,
+		    width: 530,
+      		modal: true,
+      		resizable: true,
+      		closeOnEscape: true,
+      		
+      	});
+	});
+
+
 	$('.my-slider').unslider({
 		autoplay: true,
 		arrows: true,
 		infinite: true
 	});
 
-	var $grid = $('.grid').masonry({
+	var $grid = $(".grid").masonry({
 		itemSelector: '.grid-item',
 	 	columnWidth: 0,
 	 	transitionDuration: '0.8s'
 	});
 
+	$(".btn_trailer").click(function(){
+		var id = $(this).attr("id");
+		$(".trailer"+id ).dialog({
+		    height: 800,
+		    width: 1000,
+      		modal: true,
+      		resizable: true,
+      		closeOnEscape: true,
+      	});
+	})
+
+
 	$( ".leer_mas" ).click(function() {
 		
 		var id = $(this).attr("id");
-		//alert(id);
-		if($(this).attr("value") == "Leer +"){
-			$(this).attr("value","Leer -");
-			}
+
+		if( $(this).attr( "value" ) == "Leer +" ){
+			
+			$(this).attr( "value","Leer -" );
+		
+		}
 		else{
-			$(this).attr("value","Leer +");
+			$( this ).attr( "value","Leer +" );
 			};
-  		$("#" + id + " p").toggle();
-  		$grid.masonry();
+
+  		$( "#" + id + " p" ).toggle( "slow", function(){
+
+  			$grid.masonry();
+  		} );
 	});
 
-	    $( "#estreno" ).datepicker({
+
+	
+		
+
+	/*
+	$( "#estreno" ).datepicker({
       		changeMonth: true,
       		changeYear: true
-    	});
-		
+   	});
+	*/
+
 });
